425a9c05087610cb1bd0f9eda41baa2e6c3baa86
Xun Li <xunli@uchicago.edu>
2020-05-06 13:54:19 -0700

    cleanup

--- cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xunli/github/libgeoda_test/googletest
-# Build directory: /Users/xunli/github/libgeoda_test/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
